917cfe1da5dcb49efa0c9c0705eb0fb964f90517
Neider <napandalesr@gmail.com>
2024-12-13 16:14:55 -0500

    Se configura analitycs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Providers from "@/containers/Providers";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Providers>
+    <GoogleAnalytics GA_MEASUREMENT_ID={'G-4ZXVH1BG39'} />
     <Component {...pageProps} />
   </Providers>;
 }
