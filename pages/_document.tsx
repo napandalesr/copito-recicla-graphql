0318092898f2bc50b6378f02fcc8ae30a9a3f9c2
Neider <napandalesr@gmail.com>
2024-12-16 10:01:19 -0500

    Se agrega la metaetiqueta del logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="es">
       <Head />
       <title>Copito recicla</title>
+      <link rel="icon" href="/images/copito-log.ico" media="(prefers-color-scheme: dark)"/>
+      <link rel="icon" href="/images/copito-log.ico" media="(prefers-color-scheme: light)"/>
       <meta name="description" content="COPITO RECICLA, se destaca como una iniciativa ejemplar de gobernanza ambiental que busca no solo sensibilizar, sino también generar un impacto real en la sostenibilidad del Valle del Cauca."/>
       <body className="antialiased">
         <Main />
